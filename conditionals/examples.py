6be1a3ee7d3342b21c2668580c91feab0f726d29
Adekola Aderonmu <gabkola94@gmail.com>
2022-05-25 19:45:43 +0100

    latest update

--- conditionals/examples.py
+++ conditionals/examples.py
@@ -88,8 +88,8 @@ else:
 ###############################################################################
 ##############################################################################
 #numbers = (1, 2, 3, 4)
-
-even_no = ('2', '4', '6', '8')
+'''
+even_no = ('2', '4', '6', '8') 
 odd_no = ('1', '3', '5', '7')
 
 hx = input("input any number? ")
@@ -102,7 +102,7 @@ else:
     print("Number don't exist") 
  #########################################################################################
  ##########################################################################################   
-    
+'''    
     #eg5:
 # a = 2
 # b = 5
@@ -113,7 +113,53 @@ else:
 
 #eg6:
 
-# 5
+# i = 1
+# while i < 100:
+#   #print(i)
+# #i += 1
+
+for numbs in range(0, 101, 1):
+    print(numbs)
+    
+    print('\n\n\n')
+    
+print('\n\n\n\n\n')    
+    
+# num = int(input("   INPUT ANY NUMBER TO KNOW IF ITS EVEN NUMBER OR ODD NUMBER: "))
+# if (num not in numbs):
+#     print("Number is not within range, try another. ")
+# elif (num % 2 ) == 0:
+#     print("  THIS IS AN EVEN  NUMBER! NO DOUBT. ")
+# else:
+#     print("  OoopS, WE GOT AN ODD NUMBER. ")         
+
+#eg7:
+
+print('\n\n\n\n\n')    
+    
+# num = int(input("   INPUT ANY NUMBER TO KNOW IF ITS EVEN NUMBER OR ODD NUMBER: "))
+# if (num not in range(0, 10000)):
+#     print(" >>>>Number is not within range, try another. ")
+# elif (num % 2 ) == 0:
+#     print(" >>>>THIS IS AN EVEN  NUMBER! NO DOUBT. ")
+# else:
+#     print(" >>>> OoopS, WE GOT AN ODD NUMBER. ")   
+    
+    
+#######################################################################################################
+#pw = "123"
+
+while True:
+    
+    dox = input(" INPUT PASSWORD: ")
+    if dox == "123":
+        print(" SUCCESSFUL ") 
+    break
+else:
+    print(" WRONG PASSWORD, TRY AGAIN")       
+    
+ ###################################################################################         
+
 
  
      
